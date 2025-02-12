20dbbfa530c67d1b740c579c1d19ee7110f04fa8
Amirhossein Kalari <hosein.kalari.1387@gmail.com>
2025-02-12 16:36:16 +0330

    feat(helpers): create helper functions to load views and partials easily

--- helpers.php
+++ helpers.php
@@ -10,3 +10,25 @@ function base_path(string $path = ""): string
 {
   return __DIR__ . $path;
 }
+
+/**
+ * Load a view
+ *
+ * @param string $view_name
+ * @return void
+ */
+function load_view(string $view_name): void
+{
+  require base_path("views/{$view_name}.view.php");
+}
+
+/**
+ * Load a partial
+ *
+ * @param string $partial_name
+ * @return void
+ */
+function load_partial(string $partial_name): void 
+{
+  require base_path("views/partials/{$partial_name}.php");
+}
