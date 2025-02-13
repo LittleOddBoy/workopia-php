a20040ed42adbf6ff5906d1fbf600e83e5c11d6f
Amirhossein Kalari <hosein.kalari.1387@gmail.com>
2025-02-13 07:46:54 +0330

    ref: put routes and router in separate files

--- public/index.php
+++ public/index.php
@@ -1,18 +1,6 @@
 <?php
 require '../helpers.php';
 
-
-$routes = [
-  "/" => "controllers/home.php",
-  "/listings" => "controllers/listing/index.php",
-  "/listings/create" => "controllers/listing/create.php",
-  "404" => "controllers/error/404.php"
-];
-
 $uri = $_SERVER['REQUEST_URI'];
 
-if (array_key_exists($uri, $routes)) {
-  require base_path($routes[$uri]);
-} else {
-  require base_path($routes['404']);
-}
+require base_path('router.php');
---
+++ routes.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+  "/" => "controllers/home.php",
+  "/listings" => "controllers/listing/index.php",
+  "/listings/create" => "controllers/listing/create.php",
+  "404" => "controllers/error/404.php"
+];
