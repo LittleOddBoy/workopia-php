ecfa1a50c9f295a269c0e996705eb9cf068f2606
Amirhossein Kalari <hosein.kalari.1387@gmail.com>
2025-02-13 07:47:29 +0330

    fix: send 404 page with correct http code

--- router.php
+++ router.php
@@ -5,5 +5,6 @@ $routes = require base_path('routes.php');
 if (array_key_exists($uri, $routes)) {
   require base_path($routes[$uri]);
 } else {
+  http_response_code(404);
   require base_path($routes['404']);
 }
