6c34b2e0ddc02cb1eeb737c3393a516bec61f1bc
Amirhossein Kalari <hosein.kalari.1387@gmail.com>
2025-02-20 15:15:05 +0330

    feat(Database): modify `query` to bind safe parameters to the query

--- Database.php
+++ Database.php
@@ -30,13 +30,20 @@ class Database
    * Query on the database
    * 
    * @param string $query 
+   * @param array $params - parameters that are going to be passed through execution
    * @return PDOStatement
    * @throws PDOException
    */
-  public function query(string $query): PDOStatement
+  public function query(string $query, array $params = []): PDOStatement
   {
     try {
       $statement = $this->conn->prepare($query);
+
+      // bind named parameters
+      foreach ($params as $param => $value) {
+        $statement->bindValue(':' . $param, $value);
+      }
+
       $statement->execute();
       return $statement;
     } catch (PDOException $e) {
