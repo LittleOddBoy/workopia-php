b21a2b95db7314a0b3e51a2169b4f9f5bfd02154
Amirhossein Kalari <hosein.kalari.1387@gmail.com>
2025-02-20 14:20:38 +0330

    feat(home.view): fetch data from db and show listings in home view

--- controllers/home.php
+++ controllers/home.php
@@ -2,7 +2,7 @@
 $config = require base_path('config/db.php');
 $db = new Database($config);
 
-$listings = $db->query('SELECT * FROM listings LIMIT 2')->fetchAll();
+$listings = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
 
 load_view("home", [
   "listings" => $listings,
