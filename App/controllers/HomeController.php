b27aed00f8740683a0121f698d4202b814adf4f4
Amirhossein Kalari <hosein.kalari.1387@gmail.com>
2025-02-24 11:10:09 +0330

    ref(Home): define class-based controller for home

---
+++ App/controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controllers;
+
+use Framework\Database;
+
+class HomeController
+{
+  protected $db;
+  public function __construct()
+  {
+    $config = require base_path('config/db.php');
+    $this->db = new Database($config);
+  }
+
+  /**
+   * The home page
+   *
+   * @return void
+   */
+  public function index()
+  {
+    $listings = $this->db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
+
+    load_view("home", [
+      "listings" => $listings,
+    ]);
+  }
+}
