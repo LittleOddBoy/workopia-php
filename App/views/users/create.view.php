a5a7dd6ad02420d34d5bf386a13909e3f8366b67
Amirhossein Kalari <hosein.kalari.1387@gmail.com>
2025-02-25 12:52:58 +0330

    ref: load `errors` partial to display potential error messages

--- App/views/users/create.view.php
+++ App/views/users/create.view.php
@@ -5,6 +5,9 @@
 <div class="flex justify-center items-center mt-20">
   <div class="bg-white p-8 rounded-lg shadow-md w-full md:w-500 mx-6">
     <h2 class="text-4xl text-center font-bold mb-4">Register</h2>
+    <?= load_partial('errors', [
+      'errors' => $errors ?? [],
+    ]) ?>
     <form method="POST" action="/auth/register">
       <div class="mb-4">
         <input
@@ -25,13 +28,6 @@
           type="text"
           name="city"
           placeholder="City"
-          class="w-full px-4 py-2 border rounded focus:outline-none" />
-      </div>
-      <div class="mb-4">
-        <input
-          type="text"
-          name="state"
-          placeholder="State"
           class="w-full px-4 py-2 border rounded focus:outline-none" />
       </div>
       <div class="mb-4">
